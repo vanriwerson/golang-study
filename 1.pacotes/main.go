99a2468364c3d1431fcb523a299ebbea134a031a
Bruno Riwerson Silva <riwerson@gmail.com>
2022-09-27 18:09:35 -0300

    feat (aula1): utilizando pacotes externos

--- 1.pacotes/main.go
+++ 1.pacotes/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	// Pacote externo instalado com go get github.com/badoux/checkmail
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	// Invocam-se os modulos com o nome que está após a última barra
+	erro := checkmail.ValidateFormat("user@gmail.com")
+	fmt.Println(erro) // retorna <nil> (nulo)
 }
 
 // go build cria um binário a partir do módulo atual

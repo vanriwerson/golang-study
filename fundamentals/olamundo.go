2c30ebc1b79efd77c0849cdbfcc74f2f935c9680
Bruno Riwerson Silva <riwerson@gmail.com>
2022-09-27 17:15:51 -0300

    feat: hello world in go

---
+++ fundamentals/olamundo.go
@@ -0,0 +1,9 @@
+package main // informa ao go que esse arquivo é um executável
+
+import (
+	"fmt"
+)
+
+func main() { // um executável go exige o pkg main e uma função main
+	fmt.Println("Olá, mundo!")
+}

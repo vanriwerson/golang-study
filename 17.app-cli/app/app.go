58c1584cca97d53d550f84f7a04c809a8af679a1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-03 14:02:10 -0300

    aula17 (feat): listando servidores por host

--- 17.app-cli/app/app.go
+++ 17.app-cli/app/app.go
@@ -15,17 +15,25 @@ func Gerar() *cli.App {
 	app.Name = "Exemplo de aplicação CLI em Golang"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "github.com", // valor default caso a flag não seja fornecida
+		},
+	}
+
 	app.Commands = []cli.Command{ // configuração da execução do CLI
 		{ // configura a invocação da Action:
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com",
-				},
-			}, // go run main.go ip --host github.com
-			Action: buscarIPs,
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
+			Action: buscarIPs, // executada com: go run main.go ip --host amazon.com.br
+		},
+		{
+			Name:   "server",
+			Usage:  "Busca o nome de servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores, // executada com: go run main.go server --host amazon.com.br
 		},
 	}
 
@@ -42,5 +50,18 @@ func buscarIPs(c *cli.Context) {
 
 	for _, ip := range ips { // imprime os IPs encontrados
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host) // Name Server
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }

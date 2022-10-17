1ed3ec8e11377c0e6daef039d9f3ef3601819540
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-17 09:28:12 -0300

    aula24 (tópico2): inserindo dados

--- 02.Aplied-Fundamentals/24.CRUD/main.go
+++ 02.Aplied-Fundamentals/24.CRUD/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crud/servidor"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +13,7 @@ import (
 // go get github.com/gorilla/mux
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost) // evita erros de digitação
 
 	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
---
+++ 02.Aplied-Fundamentals/24.CRUD/servidor/servidor.go
@@ -0,0 +1,58 @@
+package servidor
+
+import (
+	"crud/banco"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type usuario struct {
+	ID    uint32 `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
+}
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	novoUsuario, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Falha ao ler o corpo da requisição"))
+		return
+	}
+
+	var usuario usuario
+	if erro = json.Unmarshal(novoUsuario, &usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuario para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("INSERT INTO `devbook`.`usuarios` (nome, email) VALUES (?, ?)")
+	if erro != nil {
+		w.Write([]byte("Erro na criação do Prepared Statement"))
+		return
+	}
+	defer statement.Close()
+
+	insercao, erro := statement.Exec(usuario.Nome, usuario.Email)
+	if erro != nil {
+		w.Write([]byte("Erro na execução do Prepared Statement"))
+		return
+	}
+
+	idInserido, erro := insercao.LastInsertId()
+	if erro != nil {
+		w.Write([]byte("Erro ao obter o id inserido"))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", idInserido)))
+}

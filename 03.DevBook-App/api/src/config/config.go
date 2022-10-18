f478822a0a1c8c45cbeee3e1d4610edf6424874e
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-18 12:47:49 -0300

    feat: criando pacote config (env vars)

---
+++ 03.DevBook-App/api/src/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	StringConexaoBanco = ""
+	Porta              = 0
+)
+
+// Inicializa as variáveis de ambiente
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 5000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_ROOT_PASSWORD"),
+		os.Getenv("MYSQL_DB"),
+	)
+}

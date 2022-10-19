7d22b51fa2ae315691cb37333fce1755886bfdad
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-19 17:35:26 -0300

    feat: criando seeder para a tabela usuarios

---
+++ 03.DevBook-App/api/sql/seeders.sql
@@ -0,0 +1,8 @@
+INSERT INTO devbook.usuarios (nome, nick, email, senha)
+VALUES ("Adalberto", "Dadá", "adalberto@email.com", "superSenhaSecreta1"),
+("Giovane", "Gigio", "giovane@email.com", "superSenhaSecreta2"),
+("Jessy", "Jess", "damasceno@email.com", "superSenhaSecreta3"),
+("Rafael", "Primo", "rafael@email.com", "superSenhaSecreta4"),
+("Maristela", "Mari", "maristela@email.com", "superSenhaSecreta5"),
+("Ana", "Aninha", "anahelena@email.com", "superSenhaSecreta6"),
+("Clair", "JojoDuck", "clair@email.com", "superSenhaSecreta7");

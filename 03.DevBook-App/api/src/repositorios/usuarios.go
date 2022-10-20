47af990d962ab47c9c0eb36bac1db923d6d03598
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-20 09:48:40 -0300

    feat: exclusão de usuário

--- 03.DevBook-App/api/src/repositorios/usuarios.go
+++ 03.DevBook-App/api/src/repositorios/usuarios.go
@@ -102,3 +102,19 @@ func (repositorio Usuarios) Atualizar(usuarioID uint64, usuario modelos.Usuario)
 
 	return nil
 }
+
+func (repositorio Usuarios) Deletar(usuarioID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"DELETE FROM devbook.usuarios WHERE id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID); erro != nil {
+		return erro
+	}
+
+	return nil
+}

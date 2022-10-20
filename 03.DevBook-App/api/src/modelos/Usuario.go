271103efb93f947428437e57c9a3a58aedd2eca1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-20 07:34:33 -0300

    feat: criando validações para novos usuarios

--- 03.DevBook-App/api/src/modelos/Usuario.go
+++ 03.DevBook-App/api/src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"` // omit empty faz com que o campo não seja passado para o JSON caso esteja vazio
@@ -9,4 +13,39 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("o nome é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("o nick é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("o email é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("a senha é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }

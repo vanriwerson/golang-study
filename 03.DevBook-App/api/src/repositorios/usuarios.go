229e2311b4a66f87917524413bc583268a638896
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-20 08:00:45 -0300

    feat: buscando usuarios por nome ou nick

--- 03.DevBook-App/api/src/repositorios/usuarios.go
+++ 03.DevBook-App/api/src/repositorios/usuarios.go
@@ -3,6 +3,7 @@ package repositorios
 import (
 	"api/src/modelos"
 	"database/sql"
+	"fmt"
 )
 
 type Usuarios struct {
@@ -33,4 +34,32 @@ func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
 	}
 
 	return uint64(ultimoIdInserido), nil
+}
+
+func (repositorio Usuarios) Buscar(nomeOuNick string) ([]modelos.Usuario, error) {
+	nomeOuNick = fmt.Sprintf("%%%s%%", nomeOuNick) // %nomeOuNick%
+
+	linhas, erro := repositorio.db.Query(
+		"SELECT id, nome, nick, email, criadoEm FROM devbook.usuarios WHERE nome LIKE ? OR nick LIKE ?",
+		nomeOuNick, nomeOuNick,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+	for linhas.Next() {
+		var usuario modelos.Usuario
+
+		if erro = linhas.Scan(
+			&usuario.ID, &usuario.Nome, &usuario.Nick, &usuario.Email, &usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, usuario) // usuarios.push(usuario)
+	}
+
+	return usuarios, nil
 }

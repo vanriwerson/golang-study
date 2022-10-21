f2f18d5d05a1a2fd8561dffe7df51f8461da2065
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-21 08:38:39 -0300

    feat: gerando chave segura para o jwt

--- 03.DevBook-App/api/main.go
+++ 03.DevBook-App/api/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"api/src/config"
 	"api/src/router"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+func init() { // Usado para gerar o JWT_SECRET com o padrão recomendado
+	chave := make([]byte, 64)
+
+	if _, erro := rand.Read(chave); erro != nil {
+		log.Fatal(erro)
+	}
+
+	secret := base64.StdEncoding.EncodeToString(chave)
+	fmt.Println(secret)
+}
 
 func main() {
 	config.Carregar()

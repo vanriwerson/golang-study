08d72c1ba5ff952bc85fe27ae4e9686eee73578c
Bruno Riwerson Silva <riwerson@gmail.com>
2022-11-03 14:09:49 -0300

    feat: seguir usuário

--- 03.DevBook-App/api/src/repositorios/usuarios.go
+++ 03.DevBook-App/api/src/repositorios/usuarios.go
@@ -139,3 +139,19 @@ func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 
 	return usuario, nil
 }
+
+func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"INSERT IGNORE INTO devbook.seguidores (usuario_id, seguidor_id) VALUES (?, ?)",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}

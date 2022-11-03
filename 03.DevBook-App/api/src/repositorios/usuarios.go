869a93a288dcbca4f4042395b3dd0b2d3bdf00c7
Bruno Riwerson Silva <riwerson@gmail.com>
2022-11-03 14:19:53 -0300

    feat: deixar de seguir usuário

--- 03.DevBook-App/api/src/repositorios/usuarios.go
+++ 03.DevBook-App/api/src/repositorios/usuarios.go
@@ -155,3 +155,19 @@ func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
 
 	return nil
 }
+
+func (repositorio Usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"DELETE FROM devbook.seguidores WHERE usuario_id = ? seguidor_id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}

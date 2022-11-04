e65fdcbdbf52c155c0d2f76a782066507cb0b1bd
Bruno Riwerson Silva <riwerson@gmail.com>
2022-11-04 13:26:24 -0300

    feat: login controller

---
+++ 03.DevBook-App/webapp/src/controllers/login.go
@@ -0,0 +1,10 @@
+package controllers
+
+import (
+	"net/http"
+	"webapp/src/utils"
+)
+
+func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "login.html", nil)
+}

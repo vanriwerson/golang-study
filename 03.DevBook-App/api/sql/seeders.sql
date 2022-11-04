54ebe0cd6c81b6bb9ceb1bb46e3084c105b82e8e
Bruno Riwerson Silva <riwerson@gmail.com>
2022-11-04 08:21:25 -0300

    chore: adicionando seguidores e publicações aos seeders

--- 03.DevBook-App/api/sql/seeders.sql
+++ 03.DevBook-App/api/sql/seeders.sql
@@ -10,4 +10,11 @@ VALUES ("Adalberto", "Dadá", "adalberto@email.com", "$2a$10$jSgHypvvykWC4jJcFzK
 ("Bruno", "Riwerson", "bruno@email.com", "$2a$10$tU2.Ka7nlOAafo1u/gpLiOpEIDhCNnPqoH2AM0KbuNF7Ys4kmaT2K"); # senha: admin
 
 INSERT INTO devbook.seguidores (usuario_id, seguidor_id)
-VALUES (1, 3), (1, 4), (3, 1), (3, 4), (5, 6), (6, 5);
+VALUES (1, 3), (1, 4), (3, 1), (3, 4), (5, 6), (6, 5), (9, 1),
+(9, 2), (9, 3), (9, 4), (9, 5), (9, 6), (9, 7), (9, 8);
+
+INSERT INTO devbook.publicacoes (titulo, conteudo, autor_id)
+VALUES ("O Canadá nos espera!", "Bora estudar pra morar em Montreal!", 1),
+("#teamFrontend", "Venha para o lado TailWind da força! o/", 4),
+("Hoje está fresquinho", "40°C em Dubai ^^", 5),
+("Café com o Leite", "Nosso programa começa em breve.", 8);

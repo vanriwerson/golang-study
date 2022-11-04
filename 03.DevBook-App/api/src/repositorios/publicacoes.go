80827abe3b043c79f4d35005feb7f0725ffe7df9
Bruno Riwerson Silva <riwerson@gmail.com>
2022-11-04 07:26:20 -0300

    feat: repositório de publicações

---
+++ 03.DevBook-App/api/src/repositorios/publicacoes.go
@@ -0,0 +1,36 @@
+package repositorios
+
+import (
+	"api/src/modelos"
+	"database/sql"
+)
+
+type Publicacoes struct {
+	db *sql.DB
+}
+
+func NovoRepositorioDePublicacoes(db *sql.DB) *Publicacoes {
+	return &Publicacoes{db}
+}
+
+func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, error) {
+	statement, erro := repositorio.db.Prepare(
+		"INSERT INTO devbook.publicacoes (titulo, conteudo, autor_id) VALUES (?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacao.AutorID)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIdInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIdInserido), nil
+}

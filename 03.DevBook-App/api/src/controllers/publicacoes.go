ee4416229bf3aa116e206ee8cb42824ab54f30d2
Bruno Riwerson Silva <riwerson@gmail.com>
2022-11-04 07:30:07 -0300

    feat: criação de publicações

--- 03.DevBook-App/api/src/controllers/publicacoes.go
+++ 03.DevBook-App/api/src/controllers/publicacoes.go
@@ -1,8 +1,54 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
-func CriarPublicacao(w http.ResponseWriter, r *http.Request) {}
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoRequest, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacaoID, erro := repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	publicacao.ID = publicacaoID
+	respostas.JSON(w, http.StatusCreated, publicacao)
+}
 
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {}
 

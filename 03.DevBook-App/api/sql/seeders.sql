9ca3b791a4463c9cf05afff850cff589e575a412
Bruno Riwerson Silva <riwerson@gmail.com>
2022-11-04 08:44:08 -0300

    chore: ajustando seguindo, seguidores e publicações do admin

--- 03.DevBook-App/api/sql/seeders.sql
+++ 03.DevBook-App/api/sql/seeders.sql
@@ -10,11 +10,13 @@ VALUES ("Adalberto", "Dadá", "adalberto@email.com", "$2a$10$jSgHypvvykWC4jJcFzK
 ("Bruno", "Riwerson", "bruno@email.com", "$2a$10$tU2.Ka7nlOAafo1u/gpLiOpEIDhCNnPqoH2AM0KbuNF7Ys4kmaT2K"); # senha: admin
 
 INSERT INTO devbook.seguidores (usuario_id, seguidor_id)
-VALUES (1, 3), (1, 4), (3, 1), (3, 4), (5, 6), (6, 5), (9, 1),
-(9, 2), (9, 3), (9, 4), (9, 5), (9, 6), (9, 7), (9, 8);
+VALUES (1, 3), (1, 4), (3, 1), (2, 3), (3, 4), (5, 6), (6, 5),
+(9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6), (9, 7), (9, 8),
+(1, 9), (2, 9), (3, 9), (4, 9), (5, 9), (6, 9), (7, 9), (8, 9);
 
 INSERT INTO devbook.publicacoes (titulo, conteudo, autor_id)
 VALUES ("O Canadá nos espera!", "Bora estudar pra morar em Montreal!", 1),
 ("#teamFrontend", "Venha para o lado TailWind da força! o/", 4),
 ("Hoje está fresquinho", "40°C em Dubai ^^", 5),
-("Café com o Leite", "Nosso programa começa em breve.", 8);
+("Café com o Leite", "Nosso programa começa em breve.", 8),
+("Estudando Golang", "Louco para entrar no #teamBackend!", 9);

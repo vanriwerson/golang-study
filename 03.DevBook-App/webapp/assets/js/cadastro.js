3a49c5c8a435169756b8d909cf88ee7a0932bcd2
Bruno Riwerson Silva <riwerson@gmail.com>
2023-02-14 14:55:36 -0300

    feat: aplicação do pacote de respostas com tratamento de status 400 ou maior

--- 03.DevBook-App/webapp/assets/js/cadastro.js
+++ 03.DevBook-App/webapp/assets/js/cadastro.js
@@ -17,5 +17,10 @@ function criarUsuario(evento) {
       nick: $('#nick').val(),
       senha: $('#senha').val()
     }
+  }).done(function() {
+    alert("Usuário cadastrado com sucesso.")
+  }).fail(function(erro) {
+    console.log(erro);
+    alert("Erro ao cadastrar usuário.")
   });
 }

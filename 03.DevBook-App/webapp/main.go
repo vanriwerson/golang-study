ec9a0d154e0df67b91320b6b54792b46a0da52f4
Bruno Riwerson Silva <riwerson@gmail.com>
2023-02-24 08:06:42 -0300

    refactor: aplicando variáveis de ambiente

--- 03.DevBook-App/webapp/main.go
+++ 03.DevBook-App/webapp/main.go
@@ -14,6 +14,6 @@ func main() {
 	utils.CarregarTemplates()
 	r := router.Gerar()
 
-	fmt.Println("Escutando na porta 3000")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	fmt.Printf("Escutando na porta %d\n", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }

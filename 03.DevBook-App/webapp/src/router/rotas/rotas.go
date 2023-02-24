cf61308a53d5e64d9860a273ee1974f553780831
Bruno Riwerson Silva <riwerson@gmail.com>
2023-02-24 09:29:41 -0300

    feat: criando middleware de autenticação

--- 03.DevBook-App/webapp/src/router/rotas/rotas.go
+++ 03.DevBook-App/webapp/src/router/rotas/rotas.go
@@ -3,6 +3,8 @@ package rotas
 import (
 	"net/http"
 
+	"webapp/src/middlewares"
+
 	"github.com/gorilla/mux"
 )
 
@@ -19,7 +21,13 @@ func Configurar(router *mux.Router) *mux.Router {
 	rotas = append(rotas, rotaPaginaPrincipal)
 
 	for _, rota := range rotas {
-		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+		if rota.RequerAutenticacao {
+			router.HandleFunc(rota.URI,
+				middlewares.Logger(middlewares.Autenticar(rota.Funcao)),
+			).Methods(rota.Metodo)
+		} else {
+			router.HandleFunc(rota.URI, middlewares.Logger(rota.Funcao)).Methods(rota.Metodo)
+		}
 	}
 
 	fileServer := http.FileServer(http.Dir("./assets/"))

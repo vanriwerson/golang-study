79bd361329dfc7f387973469a69cfddf2381a716
Bruno Riwerson Silva <riwerson@gmail.com>
2023-03-22 09:49:13 -0300

    feat: descurtir publicação

--- 03.DevBook-App/webapp/assets/js/publicacoes.js
+++ 03.DevBook-App/webapp/assets/js/publicacoes.js
@@ -1,5 +1,6 @@
 $('#nova-publicacao').on('submit', criarPublicacao);
-$('.curtir-publicacao').on('click', curtirPublicacao);
+$(document).on('click', '.curtir-publicacao', curtirPublicacao);
+$(document).on('click', '.descurtir-publicacao', descurtirPublicacao);
 
 function criarPublicacao(evento) {
   evento.preventDefault();
@@ -33,11 +34,41 @@ function curtirPublicacao(evento) {
     const contadorDeCurtidas = elementoClicado.next('span');
     const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
 
-    contadorDeCurtidas.text(quantidadeDeCurtidas +1);
+    contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+
+    elementoClicado.addClass('descurtir-publicacao');
+    elementoClicado.addClass('text-danger');
+    elementoClicado.removeClass('curtir-publicacao');
   }).fail(function() {
     alert('Erro ao curtir publicação!')
   }).always(function () {
     elementoClicado.prop('disabled', false);
   })
+}
 
+function descurtirPublicacao(evento) {
+  evento.preventDefault();
+
+  const elementoClicado = $(evento.target) 
+  const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+  elementoClicado.prop('disabled', true);
+
+  $.ajax({
+    url: `/publicacoes/${publicacaoId}/descurtir`,
+    method: 'POST'
+  }).done(function() {
+    const contadorDeCurtidas = elementoClicado.next('span');
+    const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+    contadorDeCurtidas.text(quantidadeDeCurtidas - 1);
+
+    elementoClicado.addClass('curtir-publicacao');
+    elementoClicado.removeClass('text-danger');
+    elementoClicado.removeClass('descurtir-publicacao');
+  }).fail(function() {
+    alert('Erro ao descurtir publicação!')
+  }).always(function () {
+    elementoClicado.prop('disabled', false);
+  })
 }

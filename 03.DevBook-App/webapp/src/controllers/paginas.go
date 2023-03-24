c91e5f170387ed5caeebd56d695a30e18428e1b0
Bruno Riwerson Silva <riwerson@gmail.com>
2023-03-24 10:39:52 -0300

    feat: validando usuário logado

--- 03.DevBook-App/webapp/src/controllers/paginas.go
+++ 03.DevBook-App/webapp/src/controllers/paginas.go
@@ -17,6 +17,12 @@ import (
 )
 
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Ler(r)
+
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", http.StatusFound)
+	}
+
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 

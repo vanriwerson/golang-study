b1d2bd756704c5e02d3865377f1cd38064953905
Bruno Riwerson Silva <riwerson@gmail.com>
2023-03-24 09:22:37 -0300

    feat: deletar publicação

--- 03.DevBook-App/webapp/assets/js/publicacoes.js
+++ 03.DevBook-App/webapp/assets/js/publicacoes.js
@@ -4,6 +4,7 @@ $(document).on('click', '.curtir-publicacao', curtirPublicacao);
 $(document).on('click', '.descurtir-publicacao', descurtirPublicacao);
 
 $('#atualizar-publicacao').on('click', atualizarPublicacao);
+$('.deletar-publicacao').on('click', deletarPublicacao);
 
 function criarPublicacao(evento) {
   evento.preventDefault();
@@ -94,5 +95,26 @@ function atualizarPublicacao() {
     alert("Erro ao editar publicação.");
   }).always(function() {
     $('#atualizar-publicacao').prop('disabled', false);
+  });
+}
+
+function deletarPublicacao(evento) {
+  evento.preventDefault();
+
+  const elementoClicado = $(evento.target);
+  const publicacao = elementoClicado.closest('div');
+  const publicacaoId = publicacao.data('publicacao-id');
+
+  elementoClicado.prop('disabled', true);
+
+  $.ajax({
+      url: `/publicacoes/${publicacaoId}`,
+      method: "DELETE"
+  }).done(function() {
+      publicacao.fadeOut("slow", function() {
+          $(this).remove();
+      });
+  }).fail(function() {
+      alert("Erro ao excluir a publicação!");
   });
 }

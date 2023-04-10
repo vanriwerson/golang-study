f70472fbf3a21c7fa346e9ccc508448cce35d202
Bruno Riwerson Silva <riwerson@gmail.com>
2023-04-10 11:23:57 -0300

    feat: perfil do usuario logado

--- 03.DevBook-App/webapp/src/controllers/paginas.go
+++ 03.DevBook-App/webapp/src/controllers/paginas.go
@@ -148,3 +148,16 @@ func CarregarPerfilDoUsuario(w http.ResponseWriter, r *http.Request) {
 		UsuarioLogadoID: usuarioLogadoID,
 	})
 }
+
+func CarregarPerfilDoUsuarioLogado(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Ler(r)
+	usuarioID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+
+	usuario, erro := modelos.BuscarUsuarioCompleto(usuarioID, r)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	utils.ExecutarTemplate(w, "perfil.html", usuario)
+}
